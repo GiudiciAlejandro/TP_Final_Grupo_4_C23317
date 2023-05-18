42af47f1516f06759ca06d2fb2636f4ae48a72d9
Alejandro <giudicialejandro@gmail.com>
2023-05-17 23:01:18 -0300

    Form new employee with models

--- EPP_manager/applications/empleados/models.py
+++ EPP_manager/applications/empleados/models.py
@@ -4,26 +4,34 @@ from django.db import models
 # Create your models here.
 class Countries(models.Model):
     country_name = models.CharField(verbose_name="Nombre del país", max_length=75 )
+    def __str__(self):
+        return self.country_name
 
 class Doc_types(models.Model):
     doc_type_name=models.CharField(verbose_name="Tipo de documento", max_length=50)
+    def __str__(self):
+        return self.doc_type_name
 
 class Company(models.Model):
     company_name=models.CharField(verbose_name="Compañia", max_length=25)
-    company_descript = models.TextField(verbose_name="Descripción", max_length=250)
-    company_address=models.CharField(verbose_name="Dirección", max_length=250)
+    company_descript = models.TextField(verbose_name="Descripción", max_length=250, null=True, blank=True)
+    company_address=models.CharField(verbose_name="Dirección", max_length=250, null=True, blank=True)
     company_created=models.DateTimeField(verbose_name="Creado")
     company_deleted = models.DateTimeField(verbose_name="Borrado", null=True, blank=True)
+    def __str__(self):
+        return self.company_name
 
 class Worker(models.Model):
     worker_name = models.CharField(verbose_name="Nombre", max_length=50,)
     worker_surname = models.CharField(verbose_name="Apellido", max_length=50,)
-    worker_company = models.ForeignKey(Company, on_delete=models.CASCADE,verbose_name="Empresa", max_length=50,)
+    worker_company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="Empresa")
     worker_nationality = models.ForeignKey(Countries,on_delete=models.DO_NOTHING, verbose_name="Nacionalidad")
     worker_doc_type = models.ForeignKey(Doc_types,on_delete=models.DO_NOTHING, verbose_name="Tipo de documento")
     worker_doc_n = models.CharField(verbose_name="N° de documento", max_length=50,)
-    worker_state = models.BooleanField(verbose_name="Estado",)
-    worker_comments = models.TextField(verbose_name="Comentarios", max_length=500,)
+    worker_state = models.BooleanField(verbose_name="Estado")
+    worker_email= models.EmailField(verbose_name="email", null=True, blank=True)
+    worker_birthday = models.DateField(verbose_name="Fecha de nacimiento", default='1900-01-01')
+    worker_comments = models.TextField(verbose_name="Comentarios", max_length=500, null=True, blank=True)
     worker_created = models.DateTimeField(verbose_name="Creado")
     worker_deleted = models.DateTimeField(verbose_name="Borrado", null=True, blank=True)
 
--- EPP_manager/applications/empleados/views.py
+++ EPP_manager/applications/empleados/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 from django.contrib import messages
-from .form import  Employee_form
+from .form import Employee_form
+from .models import Worker, Countries, Company, Doc_types
+from datetime import datetime
 
 # Create your views here.
 
@@ -10,86 +12,86 @@ from .form import  Employee_form
 def employee_lits(request):
     # Show a list of all employees names and surname
     # Clicking in one employee open employee_details page
-    employees=[{
-        'first_name': 'Pedro',
-        'last_name': 'Del Cerro', 
-        'age': 35,
-        'nacionality': 'Argentino',
-        'company':'company1',
-        'document_type':'passport',
-        'document_number':'123456789',
-        'email':'ale@gmail.com',
-        'status': 'Active',
-        'linkto': '/empleados/employee_details/Pedro'},
-        {
-        'first_name': 'Ale',
-        'last_name': 'Del Cerro', 
-        'age': 35,
-        'nacionality': 'Argentino',
-        'company':'company1',
-        'document_type':'passport',
-        'document_number':'123456789',
-        'email':'ale@gmail.com',
-        'status': 'Active',
-        'linkto': '/empleados/employee_details/Ale'}
-    ]
-    
-    epp_1=[{'type':'Casco', 'serial_number':'csc1234', 'manufacturer':'MSA', 'asign_date':'12-4-2022'},{'type':'Anteojos', 'serial_number':'antdd53234', 'manufacturer':'North', 'asign_date':'30-1-2021'},
-    {'type':'Zapatos', 'serial_number':'boots1234', 'manufacturer':'Honeywell', 'asign_date':'2-8-2009'}]
-    
+    context = {}
+    employees = Worker.objects.all()
+    context["empleados"] = employees
 
-    context={"empleados":employees, 
-             "epps":epp_1}
-    return render(request,'employee/employee_list.html', context )
+    epp_1 = [{'type': 'Casco', 'serial_number': 'csc1234', 'manufacturer': 'MSA', 'asign_date': '12-4-2022'}, {'type': 'Anteojos', 'serial_number': 'antdd53234', 'manufacturer': 'North', 'asign_date': '30-1-2021'},
+             {'type': 'Zapatos', 'serial_number': 'boots1234', 'manufacturer': 'Honeywell', 'asign_date': '2-8-2009'}]
+
+    context["epps"]= epp_1
+    return render(request, 'employee/employee_list.html', context)
     pass
 
 
 def employee_details(request, employee):
     # Show details for specific employee
     # If logged user is admin, it will show a menu to manage employee :
-        # delete employee
-        # assign EPP
-        # deasign EPP
-        # go to EPP inspection page
+    # delete employee
+    # assign EPP
+    # deasign EPP
+    # go to EPP inspection page
     # If logged user is employee, only have the option to ask for a new EPP or report a damaged EPP
-    employee_1={
+    employee_1 = {
         'first_name': 'Pedro',
-        'last_name': 'Del Cerro', 
+        'last_name': 'Del Cerro',
         'age': 35,
         'nacionality': 'Argentino',
-        'company':'company1',
-        'document_type':'passport',
-        'document_number':'123456789',
-        'email':'ale@gmail.com',
+        'company': 'company1',
+        'document_type': 'passport',
+        'document_number': '123456789',
+        'email': 'ale@gmail.com',
         'status': 'Active',
-        'coment':'Perosna medio loca',
+        'coment': 'Perosna medio loca',
     }
+
+    epp_1 = [{'type': 'Casco', 'serial_number': 'csc1234', 'manufacturer': 'MSA', 'asign_date': '12-4-2022'}, {'type': 'Anteojos', 'serial_number': 'antdd53234', 'manufacturer': 'North', 'asign_date': '30-1-2021'},
+             {'type': 'Zapatos', 'serial_number': 'boots1234', 'manufacturer': 'Honeywell', 'asign_date': '2-8-2009'}]
     
-    epp_1=[{'type':'Casco', 'serial_number':'csc1234', 'manufacturer':'MSA', 'asign_date':'12-4-2022'},{'type':'Anteojos', 'serial_number':'antdd53234', 'manufacturer':'North', 'asign_date':'30-1-2021'},
-    {'type':'Zapatos', 'serial_number':'boots1234', 'manufacturer':'Honeywell', 'asign_date':'2-8-2009'}]
-    
-
-    context={"empleado":employee_1, 
-             "epps":epp_1}
-    return render(request,'employee/employee_details.html', context )
-    
+    context = {"empleado": employee_1,
+               "epps": epp_1}
+    return render(request, 'employee/employee_details.html', context)
 
 
-
-
-    
 def employee_new(request):
     # Show the form to load a new employee
     if request.method == "POST":
-        employee_new_form=Employee_form(request.POST) 
+        employee_new_form = Employee_form(request.POST)
         # Validaciones
         if employee_new_form.is_valid():
-            messages.add_message(request, messages.SUCCESS, 'Se han cargado correctamente los datos del nuevo empleado')        
+            # Get data from form
+            wname = employee_new_form.cleaned_data["name"]
+            wsname = employee_new_form.cleaned_data["surname"]
+            wcompany = employee_new_form.cleaned_data["company"]
+            wnationality = employee_new_form.cleaned_data["nacionality"]
+            wdoctype = employee_new_form.cleaned_data["document_type"]
+            wdoc_n = employee_new_form.cleaned_data["document_N"]
+            wcomments = employee_new_form.cleaned_data["comments"]
+            wemail = employee_new_form.cleaned_data["email"]
+            wbirthday = employee_new_form.cleaned_data["birthday"]
+            # Fill table
+            # Create new instance of model Worker
+            new_employee = Worker(
+                worker_name=wname,
+                worker_surname=wsname,
+                worker_company=wcompany,
+                worker_nationality=wnationality,
+                worker_doc_type=wdoctype,
+                worker_doc_n=wdoc_n,
+                worker_birthday = wbirthday,
+                worker_email = wemail,
+                worker_comments=wcomments,
+                worker_state=True,
+                worker_created=datetime.now()
+            )
+            new_employee.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Se han cargado correctamente los datos del nuevo empleado')
             return redirect('employee_new')
         else:
-            messages.error(request, 'Error al cargar el empleado, por favor revise los datos e intenta nuevamente')   
+            messages.error(
+                request, 'Error al cargar el empleado, por favor revise los datos e intenta nuevamente')
     else:
-        employee_new_form=Employee_form()
-    context= {'form_new_employee': employee_new_form}
-    return render(request,'employee/employee_new.html' , context)
-
+        employee_new_form = Employee_form()
+    context = {'form_new_employee': employee_new_form}
+    return render(request, 'employee/employee_new.html', context)
